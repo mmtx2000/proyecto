93d53d8bf09eba355582732e5ced6901cd864352
Luis David Richmond <luis,richmond9@gmail.com>
2020-07-10 16:26:50 -0600

    Se corriguió el spawn de los avatars y se agrega la interacción entre los avatars y la matriz

--- avatar/Avatars.py
+++ avatar/Avatars.py
@@ -17,6 +17,8 @@ fuente = pygame.font.Font('freesansbold.ttf',15)
 global Game
 Game = True
 
+""" VARIABLES DE LAS ACCIONES DE LOS AVATAR"""
+
 Arquero = False
 MoveArquero = False
 Goblin = False
@@ -25,6 +27,9 @@ Guerrero = False
 MoveGuerrero = False
 Hachero = False
 MoveHachero = False
+AtaqueArquero = True
+
+""" LISTAS DE LOS AVATARS """
 
 Arqueros = []
 Goblins = []
@@ -33,9 +38,14 @@ Hacheros = []
 
 matriz = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
 
+""" FILA DE LA MATRIZ """
+
 posFila = [42,125,220,307,390]
 
-posicion = random.choice(posFila)
+""" flecha """
+
+moveflecha = -5
+
 ########################## FUNCIONES ##########################
 
 """ calcula el tiempo de los eventos """
@@ -50,51 +60,53 @@ def Time(Game):
     global Guerrero
     global MoveGuerrero
     global Hachero
-    global MoveHachero
+    global MoveHachero,lista
     
     second = 0
         
     while Game:
             
         # DA EL PASO A CREAR EL ARQUERO 
-        if second%5 == 0 and second != 0:
+        if second%10 == 0 and second != 0 or second == 2:
 
             Arquero = True
 
-        # DA EL PASO A MOVER EL ARQUERO 
-        if second%12 == 0 and second != 0:
-
-            MoveArquero = True
-
-        # DA EL PASO A CREAR UN GOBLIN
-        if second%7 == 0 and second != 0:
-
-            Goblin = True
-
-        # DA EL PASO A MOVER EL GOBLIN
-        if second%15 == 0 and second != 0:
-
-            MoveGoblin = True
-
-        # DA EL PASO A CREAR EL GUERRERO
-        if second%9 == 0 and second != 0:
-
-            Guerrero = True
-            
-        # DA EL PASO A MOVER EL GUERRERO 
-        if second%11 == 0 and second != 0:
-
-            MoveGuerrero = True
-
         # DA EL PASO A CREAR EL HACHERO
-        if second%6 == 0 and second != 0:
+        if second%12 == 0 and second != 0 or second == 7:
 
             Hachero = True
 
-        # DA EL PASO A MOVER EL HACHERO
-        if second%9 == 0 and second != 0:
+        # DA EL PASO A CREAR UN GOBLIN
+        if second%15 == 0 and second != 0 or second == 5:
 
-            MoveHachero = True
+            Goblin = True
+
+        # DA EL PASO A CREAR EL GUERRERO
+        if second%17 == 0 and second != 0 or second == 9:
+
+            Guerrero = True
+
+        if second > 4:
+
+            # DA EL PASO A MOVER EL ARQUERO 
+            if second%5 == 0 and second != 0:
+
+                MoveArquero = True
+
+            # DA EL PASO A MOVER EL HACHERO
+            if second%13 == 0 and second != 0:
+
+                MoveHachero = True
+
+            # DA EL PASO A MOVER EL GOBLIN
+            if second%14 == 0 and second != 0:
+
+                MoveGoblin = True
+                
+            # DA EL PASO A MOVER EL GUERRERO 
+            if second%10 == 0 and second != 0:
+
+                MoveGuerrero = True
    
         second += 1
         time.sleep(1)
@@ -167,47 +179,72 @@ def MoveHacheros(i, fin):
     
     Hacheros[i].Hachero()
     return MoveHacheros(i+1, fin)
+
+def EncontrarRook(i,j):
+
+    return True
     
 def Generar():
 
     """ genera un arquero y lo mete en su respectiva lista """
     
     global Arquero, Arqueros, Goblin, Goblins, Guerrero, Guerreros,Hachero,Hacheros
+
     
+    # pos = coordenada en y de los avatar
+    pos = random.choice(posFila)
+    
+    # CALCULA LA FILA EN LA QUE APERECERÁ EL AVATAR
+    if pos == 42:
+        i = 0
+    if pos == 125:
+        i = 1
+    if pos == 220:
+        i = 2
+    if pos == 307:
+        i = 3
+    if pos == 390:
+        i = 4
+
+    # CREA UN ARQUERO Y LO METE A SU RESPECTIVA LISTA
     if Arquero:
-        Arqueros += [Avatar(5,5,Comprobar(random.choice(posFila)),593,50,avatar1)]
-        Arquero = False
+        if matriz[i][8] != 1:
+            matriz[i][8] = 1
+            Arqueros += [Avatar(arquero,5,i,8,593,pos,5)]
+            Arquero = False
+        else:
+            return
+    # CREA UN GOBLIN Y LO METE A SU RESPECTIVA LISTA
     if Goblin:
-        Goblins += [Avatar(5,5,Comprobar(random.choice(posFila)),593,50,goblin)]
-        Goblin = False
+        if matriz[i][8] != 1:
+            matriz[i][8] = 1
+            Goblins += [Avatar(goblin,5,i,8,593,pos,5)]
+            Goblin = False
+        else:
+            return
+    # CREA UN GUERRERO Y LO METE A SU RESPECTIVA LISTA
     if Guerrero:
-        Guerreros += [Avatar(5,5,Comprobar(random.choice(posFila)),593,50,guerrero)]
-        Guerrero = False
+        if matriz[i][8] != 1:
+            matriz[i][8] = 1
+            Guerreros += [Avatar(guerrero,5,i,8,593,pos,5)]
+            Guerrero = False
+        else:
+            return
+    # CREA UN HACHERO Y LO METE A SU RESPECTIVA LISTA
     if Hachero:
-        Hacheros += [Avatar(5,5,Comprobar(random.choice(posFila)),593,50,guerrero)]
-        Hachero = False
+        if matriz[i][8] != 1:
+            matriz[i][8] = 1
+            Hacheros += [Avatar(hachero,5,i,8,593,pos,5)]
+            Hachero = False
+        else:
+            return
 
-def Comprobar(nuevaposicion):
-
-    global posicion
-
-    if posicion == nuevaposicion:
-
-        return Comprobar(random.choice(posFila))
-
-    else:
-        posicion = nuevaposicion
-        return nuevaposicion
-        
 ###########################IMAGENES###########################  
 
 background = pygame.image.load("fondo1.png").convert()
 
-rook = pygame.image.load("rook.png").convert()
-rook.set_colorkey([0, 0, 0])
-
-avatar1 = pygame.image.load("avatar2.png")
-avatar1.set_colorkey([0, 0, 0])
+arquero = pygame.image.load("avatar2.png")
+arquero.set_colorkey([0, 0, 0])
 
 goblin = pygame.image.load("goblin.png")
 goblin.set_colorkey([0, 0, 0])
@@ -218,37 +255,57 @@ guerrero.set_colorkey([0, 0, 0])
 hachero = pygame.image.load("hachero.png")
 hachero.set_colorkey([0, 0, 0])
 
+flecha = pygame.image.load("flecha.png")
+flecha.set_colorkey([0, 0, 0])
+
 ########################## CLASES ############################
 
 class Avatar:
     
-    # Attack: tipo de ataque
-    # life : vida
-    # row : fila en la que aparecerá
-    # avatar: tipo de avatar con la imagen
-    # x: posicion de x
-    # range_attack: rango de ataque(distancia a la que puede atacar)
+    # avatar = imagen del avatar
+    # life = cantidad de vida
+    # i = fila de la matriz
+    # j = columna de la matriz
+    # posx = coordenadas en x
+    # posy = coordenadas en y
     
-    def __init__(self,attack,life,row,x,range_attack,avatar):
+    def __init__(self,avatar,life,i,j,x,y,ataque):
 
         """ genera los avatars"""
         
-        self.Attack = attack
-        self.Life = life
-        self.Row = row
-        self.Avatar = avatar
+        self.avatar = avatar
+        self.life = life
+        self.i = i
+        self.j = j
         self.posx = x
-        self.Range = range_attack
+        self.posy = y
+        self.ataque = ataque
         
     def Archer(self):
 
         """ muestra, ataque y mueve los arqueros """
 
+        global AtaqueArquero,moveflecha
+
         if MoveArquero:
 
-            self.posx -= 65
+            if  matriz[self.i][self.j-1] != 0:
+                
+                return 
 
-        screen.blit(avatar1,[self.posx, self.Row])
+            else:
+                self.j -= 1
+                matriz[self.i][self.j+1] -= 1 
+                self.posx-= 65
+                matriz[self.i][self.j] += 1
+
+        if AtaqueArquero:
+
+            if EncontrarRook(self.i,self.j):
+
+                screen.blit(flecha,(self.posx-50,self.posy))
+
+        screen.blit(arquero,[self.posx, self.posy])
 
     def Goblin(self):
 
@@ -256,9 +313,17 @@ class Avatar:
 
         if MoveGoblin:
 
-            self.posx-= 65
+            if  matriz[self.i][self.j-1] != 0:
+                
+                return 
 
-        screen.blit(goblin,[self.posx, self.Row])
+            else:
+                self.j -= 1
+                matriz[self.i][self.j+1] -= 1 
+                self.posx-= 65
+                matriz[self.i][self.j] += 1
+
+        screen.blit(goblin,[self.posx, self.posy])
 
     def Warrior(self):
 
@@ -266,9 +331,17 @@ class Avatar:
 
         if MoveGuerrero:
 
-            self.posx-= 65
+            if  matriz[self.i][self.j-1] != 0:
+                
+                return 
 
-        screen.blit(guerrero,[self.posx, self.Row])
+            else:
+                self.j -= 1
+                matriz[self.i][self.j+1] -= 1 
+                self.posx-= 65
+                matriz[self.i][self.j] += 1
+
+        screen.blit(guerrero,[self.posx, self.posy])
 
     def Hachero(self):
 
@@ -276,9 +349,17 @@ class Avatar:
 
         if MoveHachero:
 
-            self.posx-= 65
+            if  matriz[self.i][self.j-1] != 0:
+                
+                return 
 
-        screen.blit(hachero,[self.posx, self.Row])
+            else:
+                self.j -= 1
+                matriz[self.i][self.j+1] -= 1 
+                self.posx-= 65
+                matriz[self.i][self.j] += 1
+                
+        screen.blit(hachero,[self.posx, self.posy])
             
 
 ######################### HILOS ################################
@@ -289,10 +370,12 @@ time_second.start()
 ######################### LÓGICA DEL JUEGO #####################
 
 while Game:
+    
     MiTexto = fuente.render(str(pygame.mouse.get_pos()),True,(0,0,0),(50,50,50))
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             Game = False
             
     if Game:
